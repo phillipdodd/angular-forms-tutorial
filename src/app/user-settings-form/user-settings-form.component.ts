db34286bc948c09822cf7ef74aeed501e6ccccee
Phillip Dodd <pdodd@uga.edu>
2019-11-12 08:10:56 -0500

    init

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styles: []
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
